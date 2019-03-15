f867ea93552078b168eb19366c9dc23d97ff154e
Earleking <arek.fielding@gmail.com>
2019-03-15 12:34:22 -0400

    started pulling data from api

--- frontend/src/components/info-page/info-page.js
+++ frontend/src/components/info-page/info-page.js
@@ -5,9 +5,12 @@ import Header from '../header/header';
 import SearchBar from '../searchbar/searchbar';
 
 class InfoPage extends Component {
-	constructor(props) {
-		super(props);
-		// const id = this.props.id;
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+    componentDidMount() {
         const id = this.props.match.params.id;
         var url = 'https://kitsu.io/api/edge/anime/' + id;
         fetch(url).then((response) => {
@@ -17,41 +20,97 @@ class InfoPage extends Component {
             }
             response.json().then((data) => {
                 console.log(data);
+                this.setState({"data": data.data});
+                // this.setState({"title": data.data.attributes.canonicalTitle})
+                // console.log(this.state.data.data.attributes.canonicalTitle);
             });
         })
     }
 	render() {
-		return (
-			<div className="holder">
+        var display;
+        if(this.state.data != undefined) {
+            display = <div className="holder">
                 <Header />
                 <div className="main-box">
                     <SideBar />
                     <div className="contents">
                         <div className="sidebar-container"><SearchBar /></div>
-                        <div className="title">Title</div>
+                        <div className="title">{this.state.data.attributes.canonicalTitle}</div>
                         <hr/>
                         <div className="show-info">
                             <div className="short-info">
-                                <img className="cover-img" src="https://media.kitsu.io/anime/poster_images/1/original.jpg?1431697256" alt=""/>
+                                <img className="cover-img" src={this.state.data.attributes.posterImage.original} alt=""/>
                                 <div>
-                                    <p>Rating: 8.40</p>
-                                    <p>Members: 100,000</p>
+                                    <p>Rating: Pending</p>
+                                    <p>Popularity: {this.state.data.attributes.popularityRank}</p>
                                 </div>
                             </div>
                             <div className="studio-info">
-                                Studio Info
+                                <div className="info-header">Information</div>
+                                <table className="studio-table">
+                                    <tbody>
+                                        <tr>
+                                            <td>Type:</td>
+                                            <td>TV</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Episodes:</td>
+                                            <td>{this.state.data.attributes.episodeCount}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Status:</td>
+                                            <td>{this.state.data.attributes.status}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Aired:</td>
+                                            <td>{(new Date(this.state.data.attributes.startDate)).toDateString()}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Premired:</td>
+                                            <td>Winter 2019</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Studios:</td>
+                                            <td>Kinema Citrus</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Genres:</td>
+                                            <td>Action, Adventure, Drama, Fantasy</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Duration:</td>
+                                            <td>{this.state.data.attributes.episodeLength} min</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Popularity:</td>
+                                            <td>#297</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
                             </div>
                             <div className="summary">
-                                Synopsis
+                                <div className="info-header">Synopsis</div>
                                 <div className="summary-text">
-                                    BLAH BLAH BLAH
+                                {this.state.data.attributes.synopsis}
                                 </div>
 
                             </div>
                         </div>
+                        <div className="episodes">
+                            <div className="title">
+                                Episode List
+                            </div>
+                            <hr/>
+                        </div>
                     </div>
                 </div>
             </div>
+        }
+        else {
+            display = <div>Loading data, please hold</div>
+        }
+		return (
+			display
 		);
 	}
 }
