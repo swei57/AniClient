3053f40a2f4e16a48b93cc95e1ce5e9a48690d6a
Earleking <arek.fielding@gmail.com>
2019-03-15 13:30:24 -0400

    added image cards

--- frontend/src/components/info-page/info-page.js
+++ frontend/src/components/info-page/info-page.js
@@ -19,12 +19,57 @@ class InfoPage extends Component {
                 return;
             }
             response.json().then((data) => {
-                console.log(data);
-                this.setState({"data": data.data});
-                // this.setState({"title": data.data.attributes.canonicalTitle})
-                // console.log(this.state.data.data.attributes.canonicalTitle);
+                this.setState({"data": data.data})
+                var epUrl = url + "/episodes";
+                this.fetchEpisodes(epUrl, []);
             });
         })
+    }
+    fetchEpisodes(url, array) {
+        fetch(url).then((response) => {
+            if(response.status !== 200) {
+                console.log("Could not fetch episode");
+                return;
+            }
+            response.json().then((data) => {
+                array = array.concat(data.data);
+                if(data.links.next && array.length < 30) {
+                    this.fetchEpisodes(data.links.next, array);
+                }
+                else {
+                    this.setState({"episodes": array});
+                }
+            });
+            
+        });
+    }
+    displayEpisodes() {
+        var html = [];
+        if(this.state.episodes) {
+            for(var n in this.state.episodes) {
+                var ep = this.state.episodes[n];
+                console.log(ep);
+                
+                if(ep.attributes.thumbnail) {
+                    html.push(
+                        <div className="ep-preview">
+                            <img className="cover-img glass" src={ep.attributes.thumbnail.original} alt=""/>
+                            <div className="ep-name">Episode {+n + 1}</div>
+                        </div>
+                    )
+                }
+                else {
+                    html.push(
+                        <div className="ep-preview">
+                            <img className="cover-img glass" src="http://denrakaev.com/wp-content/uploads/2015/03/no-image-800x511.png" alt=""/>
+                            <div className="ep-name">Episode {+n + 1}</div>
+                        </div>
+                    )
+                }
+                
+            }
+        }
+        return html;
     }
 	render() {
         var display;
@@ -51,7 +96,7 @@ class InfoPage extends Component {
                                     <tbody>
                                         <tr>
                                             <td>Type:</td>
-                                            <td>TV</td>
+                                            <td>{this.state.data.attributes.subtype}</td>
                                         </tr>
                                         <tr>
                                             <td>Episodes:</td>
@@ -59,7 +104,7 @@ class InfoPage extends Component {
                                         </tr>
                                         <tr>
                                             <td>Status:</td>
-                                            <td>{this.state.data.attributes.status}</td>
+                                            <td>{this.state.data.attributes.status.charAt(0).toUpperCase() + this.state.data.attributes.status.slice(1)}</td>
                                         </tr>
                                         <tr>
                                             <td>Aired:</td>
@@ -101,6 +146,9 @@ class InfoPage extends Component {
                                 Episode List
                             </div>
                             <hr/>
+                            <div className="ep-list">
+                                {this.displayEpisodes()}
+                            </div>
                         </div>
                     </div>
                 </div>
