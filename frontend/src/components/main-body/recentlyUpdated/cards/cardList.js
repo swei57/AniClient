7f04a035241ca225c6495d515f244202080ddd3b
Franklin Liu <franklinliu@Franklins-MacBook-Pro.local>
2019-03-15 13:25:31 -0400

    created props to pass in name and image url

--- frontend/src/components/main-body/recentlyUpdated/cards/cardList.js
+++ frontend/src/components/main-body/recentlyUpdated/cards/cardList.js
@@ -4,26 +4,67 @@ import './cardList.css';
 
 class CardList extends Component {
 
+  constructor(props){
+    super(props);
+    this.state ={
+library: [{
+  name: 'Shield Hero',
+  url: 'https://cdn.myanimelist.net/images/anime/1068/97169.jpg'
+} , 
+{
+  name: 'My Hero Academia',
+  url:  'https://cdn.myanimelist.net/images/anime/10/78745.jpg'
+},
+{
+  name: 'Naruto',
+  url:'https://cdn.myanimelist.net/images/anime/13/17405.jpg'
+},
+{
+  name: 'Bleach',
+  url:'https://cdn.myanimelist.net/images/anime/3/40451.jpg'
+},
+{
+  name: 'Attack On Titan',
+  url:'https://cdn.myanimelist.net/images/anime/10/47347.jpg'
+},
+{
+  name: 'Kayuya-Sama',
+  url:'https://cdn.myanimelist.net/images/anime/1291/97023.jpg'
+},
+{
+  name: 'Dororo',
+  url:'https://cdn.myanimelist.net/images/anime/1933/97061.jpg'
+}
+,
+{
+  name: 'Mob Pyscho II',
+  url:'https://cdn.myanimelist.net/images/anime/1918/96303.jpg'
+},
+{
+  name: 'SAO',
+  url: 'https://cdn.myanimelist.net/images/anime/11/39717.jpg'
+},
+{
+  name: 'Cowboy Bebop',
+  url:'https://cdn.myanimelist.net/images/anime/4/19644.jpg'
+}]
+  }
+  }
   render() {
+
+    const cardsArray = this.state.library.map((anime) => {
+        return(
+          <Card name = {anime.name} imageURL = {anime.url}/>
+        );
+    });
     return (
-
-
     <div>
-    <h4 className = "ml-4 mt-4">Recently Updated</h4>
+    <h4 className = "ml-4 mt-4">Currently Watching</h4>
     <hr></hr>
     <div className = "container-full allItems">   
       <img className="arrow invert" src="https://cdn4.iconfinder.com/data/icons/icon-flat-icon-set/50/triangle-left-512.png" alt ="wife" />
       <div className = "cardList">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+       {cardsArray}
       </div>
       <img className="arrow flip invert" src="https://cdn4.iconfinder.com/data/icons/icon-flat-icon-set/50/triangle-left-512.png" alt ="wife"/>
       </div>
