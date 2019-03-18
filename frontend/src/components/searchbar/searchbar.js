31ff83de5768ea25cc5883e63d917aebd874c4da
swei57 <swei57@users.noreply.github.com>
2019-03-18 18:28:29 -0400

    removed underline from anchor tag, made search results to 10

--- frontend/src/components/searchbar/searchbar.js
+++ frontend/src/components/searchbar/searchbar.js
@@ -33,15 +33,14 @@ class SearchBar extends Component{
   render(){
     return(
         <div className = "p-2">
-           {/* <img src="https://www.gettyimages.com/gi-resources/images/CreativeLandingPage/HP_Sept_24_2018/CR3_GettyImages-159018836.jpg" alt=""/> */}
            <div className="test">
                <input className = "text-light searchbar" type = "search" 
-               placeholder ="search anime "
+               placeholder ="Search Anime "
                onChange =  {this.onSearchChange}></input> 
                <i className="searchIcon fas fa-search ml-3" aria-hidden="true"></i>
-                {this.state.foundShows.slice(0,5).map((show) => (
+                {this.state.foundShows.slice(0,10).map((show) => (
                     <div className = "filterShow">
-                    <a href ={`/show/${show.id}`}>{show.attributes.canonicalTitle}</a>
+                    <a className="searchRes"href ={`/show/${show.id}`}>{show.attributes.canonicalTitle}</a>
                     </div>
                 ))}
            </div>
